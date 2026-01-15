36c65b644fd50d0133d96544925f736d71e4252f
8RB88 <bustamanteroberto49@gmail.com>
2026-01-14 22:25:25 -0500

    delete

--- LumenRatingsApi/public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo json_encode([
-    'status' => 'OK',
-    'message' => 'Servidor respondiendo correctamente',
-    'time' => date('Y-m-d H:i:s'),
-    'path' => $_SERVER['REQUEST_URI']
-]);
